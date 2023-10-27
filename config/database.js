dff52a38049c4b22dac037d16091f5a4628ecaad
shazilhasan <146967418+shazilhasan@users.noreply.github.com>
2023-10-27 22:01:49 +0530

    database.js

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-	database: 'mongodb://localhost:27017/dashboard',
+	database: 'mongodb://technologics-server:oVT37P2oYORDgYwi8ouLb1wwLm0NSTEzX9qUtSdEqaMMd3OPMx4R2yufCQQRDgimCP0oldWZavahACDbetFXwg==@technologics-server.mongo.cosmos.azure.com:10255/technologics-database?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@technologics-server@',
 	secret: 'taqueopa'
 }
